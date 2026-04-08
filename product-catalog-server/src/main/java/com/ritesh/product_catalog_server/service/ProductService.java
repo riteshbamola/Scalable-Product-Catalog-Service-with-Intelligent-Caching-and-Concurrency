048a236e72e5f82132aebd8f1f21efa41fedd97e
Ritesh Bamola <rishubamola01@gmail.com>
2026-04-08 23:49:33 +0530

    Service Added for Product

--- product-catalog-server/src/main/java/com/ritesh/product_catalog_server/service/ProductService.java
+++ product-catalog-server/src/main/java/com/ritesh/product_catalog_server/service/ProductService.java
@@ -1,4 +1,33 @@
 package com.ritesh.product_catalog_server.service;
 
+import com.ritesh.product_catalog_server.db.ProductEntity;
+import com.ritesh.product_catalog_server.db.ProductRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    private ProductRepo productRepo;
+
+    ProductService(ProductRepo productRepo){
+        this.productRepo = productRepo;
+    }
+
+
+
+    public ProductEntity getProduct(Long id){
+        return productRepo.findById(id).orElseThrow(()-> new RuntimeException("Product Not Found"));
+    }
+
+    public ProductEntity addProduct(ProductEntity product) {
+        if (productRepo.existsById(product.getId())) {
+            throw new RuntimeException(
+                    "Product with ID " + product.getId() + " already exists."
+            );
+        }
+        return productRepo.save(product);
+    }
+
+
+
 }
