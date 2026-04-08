8deaba9d6ca5904d1ada994bf80f85c4b0265d09
Ritesh Bamola <rishubamola01@gmail.com>
2026-04-08 23:48:59 +0530

    Controller Added

--- product-catalog-server/src/main/java/com/ritesh/product_catalog_server/controller/ProductController.java
+++ product-catalog-server/src/main/java/com/ritesh/product_catalog_server/controller/ProductController.java
@@ -1,4 +1,44 @@
 package com.ritesh.product_catalog_server.controller;
 
+import com.ritesh.product_catalog_server.db.ProductEntity;
+import com.ritesh.product_catalog_server.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class ProductController {
+
+
+    private ProductService productService;
+
+    ProductController(ProductService productService){
+        this.productService= productService;
+    }
+
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Map<String, ProductEntity>> getProduct(@PathVariable Long id) {
+        Map<String, ProductEntity> response = new HashMap<>();
+
+        ProductEntity product = productService.getProduct(id);
+        response.put("product", product);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<Map<String, Object>> addProduct(@RequestBody ProductEntity product) {
+        ProductEntity savedProduct = productService.addProduct(product);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Product created successfully");
+        response.put("product", savedProduct);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }
