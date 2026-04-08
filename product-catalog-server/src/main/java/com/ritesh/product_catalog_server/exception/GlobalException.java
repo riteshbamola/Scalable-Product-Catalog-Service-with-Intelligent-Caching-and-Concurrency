70831811dd1d870f6eb412232063d97779c83910
Ritesh Bamola <rishubamola01@gmail.com>
2026-04-08 23:50:07 +0530

    Global Exception Added

--- product-catalog-server/src/main/java/com/ritesh/product_catalog_server/exception/GlobalException.java
+++ product-catalog-server/src/main/java/com/ritesh/product_catalog_server/exception/GlobalException.java
@@ -1,4 +1,19 @@
 package com.ritesh.product_catalog_server.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalException {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
 }
