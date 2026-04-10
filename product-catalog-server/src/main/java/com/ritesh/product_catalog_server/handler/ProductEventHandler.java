17bc08a066455f814c2682019e3b5fe43ab7f67a
Ritesh Bamola <rishubamola01@gmail.com>
2026-04-10 13:07:41 +0530

    Handker

--- product-catalog-server/src/main/java/com/ritesh/product_catalog_server/handler/ProductEventHandler.java
+++ product-catalog-server/src/main/java/com/ritesh/product_catalog_server/handler/ProductEventHandler.java
@@ -1,4 +1,20 @@
 package com.ritesh.product_catalog_server.handler;
 
+import com.ritesh.product_catalog_server.RabitMQ.Producer;
+import com.ritesh.product_catalog_server.db.ProductEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.transaction.event.TransactionPhase;
+
+@Component
 public class ProductEventHandler {
+
+    @Autowired
+    private Producer producer;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(ProductEntity product) {
+        producer.publishUpdate(product); // ✅ safe
+    }
 }
